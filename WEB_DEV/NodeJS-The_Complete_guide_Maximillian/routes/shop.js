e11f318d668b468d15b5d16ac5ef392151f99eff
Akshay <66077920+Akshay-akkay@users.noreply.github.com>
2021-03-31 12:17:15 +0530

    Templating

--- WEB_DEV/NodeJS-The_Complete_guide_Maximillian/routes/shop.js
+++ WEB_DEV/NodeJS-The_Complete_guide_Maximillian/routes/shop.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 
-const rootDir = require('../utils/path');
+// const rootDir = require('../utils/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  console.log('In the middleware 2');
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  console.log(adminData.products);
+  const { products } = adminData;
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop', { prods: products, docTitle: 'Shop', path: '/' });
 });
 
 module.exports = router;
